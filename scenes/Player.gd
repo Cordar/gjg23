5d02ac6b6c7aee52d70a50cdd71e25c81139ddc6
Cordar <enricmir1993@gmail.com>
2023-11-19 13:19:49 +0100

    added player switch and animated

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -10,15 +10,17 @@ func _physics_process(delta):
 
 	# Handle jumping
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		$PlayerHell/AnimatedSprite.play("jump")
 		jump_timer = 0.0
 		vertical_force = 35.0
 
 	# Apply gravity when not on the floor
 	if not is_on_floor():
+		$PlayerHell/AnimatedSprite.play("jump")
+		$PlayerHeaven/AnimationPlayer.play("jump")
 		transform.origin.y += delta * gravity
 	else:
 		$PlayerHell/AnimatedSprite.play("run")
+		$PlayerHeaven/AnimationPlayer.play("run")
 	vertical_force -= 60.0 * delta
 	if vertical_force < 0.0: 
 		vertical_force = 0.0
@@ -32,9 +34,11 @@ func _on_area_obstacles_body_entered(body:Node2D):
 	get_tree().reload_current_scene()
 
 func activate_hell():
+	print("hell")
 	$PlayerHeaven.visible = false
 	$PlayerHell.visible = true
 
 func activate_heaven():
+	print("heaven")
 	$PlayerHeaven.visible = true
 	$PlayerHell.visible = false
