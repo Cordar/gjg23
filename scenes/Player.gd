8f03f27fbbe55f5683f6459dfc83114b704fd0a1
Cordar <enricmir1993@gmail.com>
2023-11-19 12:00:44 +0100

    more stuff

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -10,12 +10,15 @@ func _physics_process(delta):
 
 	# Handle jumping
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		$PlayerHell/AnimatedSprite.play("jump")
 		jump_timer = 0.0
 		vertical_force = 35.0
 
 	# Apply gravity when not on the floor
 	if not is_on_floor():
 		transform.origin.y += delta * gravity
+	else:
+		$PlayerHell/AnimatedSprite.play("run")
 	vertical_force -= 60.0 * delta
 	if vertical_force < 0.0: 
 		vertical_force = 0.0
